544ab70aa1f5416f2d372840eddc29cc8b7c2085
StevenMcDonald97 <sam489@cornell.edu>
2021-07-22 20:23:13 -0700

    trying to fix css path pproblem

--- components/TestCB.js
+++ components/TestCB.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../styles/TestCB.module.css";
 
 import testCardsOriginal from "../data/testCardsCB";
 import TestCard from "../components/TestCard";
