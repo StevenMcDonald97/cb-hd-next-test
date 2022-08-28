8207959b6174eeaa48d8ab403beb12d06aa3da4d
Steven McDonald <sam489@cornell.edu>
2022-08-27 22:36:23 -0700

    improve website shard styling

--- pages/ContactPage.js
+++ pages/ContactPage.js
@@ -12,7 +12,7 @@ const ContactPage = () => {
   	<div>
 	    <MenuLink/>
 	    <Container maxWidth="lg" style={{ justifyContent: "center" }}>
-	      <h1 style={{ fontSize: "3rem", marginTop:"30px", textAlign:"center"}}>Charities Using The2Senses </h1>
+	      <h1 style={{ fontSize: "3rem", marginTop:"30px", marginRight:"10px", textAlign:"center"}}>Charities Using The2Senses </h1>
 	      <br />
 	      <div style={{ textAlign:"center"}}>
 		<div style={{width:"45%", display:"inline-block", marginTop:"30px", textAlign:"center"}}>
@@ -33,7 +33,7 @@ const ContactPage = () => {
 		</div>
 	      </div>
 	      <div style={{ textAlign:"center"}}>
-		<div style={{ verticalAlign:"top", width:"45%", display:"inline-block", marginTop:"15px", marginRight:"30px", textAlign:"center"}}>
+		<div style={{ verticalAlign:"top", paddingLeft: 8%, width:"45%", display:"inline-block", marginTop:"15px", marginRight:"30px", textAlign:"center"}}>
 			<h2 style={{  textAlign:"left"}}>Hope Public Charitable Trust</h2>
 			<p style={{ textAlign:"left"}}>
 				6b/26, Sarangapani Street, Krishnapuram, Ambattur
@@ -44,7 +44,7 @@ const ContactPage = () => {
 				<a style={{ color: "rgb(80, 183, 255)"}} href="https://hopechennai.com/our-services/"> Our Services | Hope Chennai Trust </a>
 			</div>	
 		</div>
-		<div style={{ verticalAlign:"top", width:"45%", display:"inline-block", marginTop:"15px", marginLeft:"30px", textAlign:"center"}}>
+		<div style={{ verticalAlign:"top", paddingLeft:"4%", width:"45%", display:"inline-block", marginTop:"15px", marginLeft:"30px", textAlign:"center"}}>
 			<h2 style={{ textAlign:"left" }}>Samarthanam Trust for the Disabled</h2>
 			<p style={{ textAlign:"left" }}>
 				CA:39, 15th Cross, 16th Main, Sector-4, 
