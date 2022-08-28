063ce72ee08eb2783f345951994eae7804beae0a
Steven McDonald <sam489@cornell.edu>
2022-08-27 21:22:38 -0700

    Update ContactPage.js

--- pages/ContactPage.js
+++ pages/ContactPage.js
@@ -15,20 +15,20 @@ const ContactPage = () => {
 		Charities Using The2Senses 
 	      </h2>
 
-		<h3 style={{ width: "500px", margin:"auto"; textAlign:"left" }}>Hope Public Charitable Trust</h3>
-		<p style={{ width: "500px", margin:"auto"; textAlign:"left" }}>
+		<h3 style={{ width: "500px", margin:"auto", textAlign:"left" }}>Hope Public Charitable Trust</h3>
+		<p style={{ width: "500px", margin:"auto", textAlign:"left" }}>
 			6b/26, Sarangapani Street, Krishnapuram, Ambattur
 			Chennai - 600053
 			
 		</p>
-		<a style={{ width: "500px", margin:"auto"; textAlign:"left" }} href="https://hopechennai.com/our-services/"> Our Services | Hope Chennai Trust </a>
+		<a style={{ width: "500px", margin:"auto", textAlign:"left" }} href="https://hopechennai.com/our-services/"> Our Services | Hope Chennai Trust </a>
 		<br/>
-		<h3 style={{ width: "500px", margin:"auto"; textAlign:"left" }}>Samarthanam Trust for the Disabled</h3>
-		<p style={{ width: "500px", margin:"auto"; textAlign:"left" }}>
+		<h3 style={{ width: "500px", margin:"auto", textAlign:"left" }}>Samarthanam Trust for the Disabled</h3>
+		<p style={{ width: "500px", margin:"auto", textAlign:"left" }}>
 			CA:39, 15th Cross, 16th Main, Sector-4, 
 			HSR Layout, Bangalore - 560102
 		</p>
-		<a style={{ width: "500px", margin:"auto"; textAlign:"left" }} href="https://www.samarthanam.org/about-us/"> About Us | Samarthanam Trust for the Disabled | NGO for Disabled </a> 
+		<a style={{ width: "500px", margin:"auto", textAlign:"left" }} href="https://www.samarthanam.org/about-us/"> About Us | Samarthanam Trust for the Disabled | NGO for Disabled </a> 
 	    </Container>
 	</div>
   );
