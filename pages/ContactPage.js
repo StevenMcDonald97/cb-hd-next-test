4aaa3c0a176df07694143374108b9e9f78bf75a3
Steven McDonald <sam489@cornell.edu>
2022-08-27 22:06:22 -0700

    Fix website details photos styling

--- pages/ContactPage.js
+++ pages/ContactPage.js
@@ -15,14 +15,14 @@ const ContactPage = () => {
 	      <h1 style={{ fontSize: "3rem", marginTop:"30px", textAlign:"center"}}>Charities Using The2Senses </h1>
 	      <br />
 	      <div style={{ textAlign:"center"}}>
-		<div style={{ display:"inline-block", marginTop:"30px", marginRight:"15px", textAlign:"center"}}>
+		<div style={{ maxWidth:"300px" display:"inline-block", marginTop:"30px", marginRight:"15px", textAlign:"center"}}>
 		      <Image
 			src={photo1}
 			alt="The 2 Senses being used"
 			height="250"
 		      />
 		</div>
-		<div style={{ display:"inline-block", marginTop:"30px", marginLeft:"15px", textAlign:"center"}}>
+		<div style={{ maxWidth:"300px" display:"inline-block", marginTop:"30px", marginLeft:"15px", textAlign:"center"}}>
 		      <Image
 			src={photo2}
 			alt="The 2 Senses being used"
@@ -30,25 +30,25 @@ const ContactPage = () => {
 		      />
 		</div>
 	      </div>
-	      <div>
-		<div style={{ display:"inline-block", marginTop:"15px", marginRight:"15px", width:"35%", textAlign:"center"}}>
-			<h2 style={{ width: "500px", margin:"auto", textAlign:"left"}}>Hope Public Charitable Trust</h2>
-			<p style={{ width: "500px", margin:"auto", textAlign:"left"}}>
+	      <div style={{ textAlign:"center"}}>
+		<div style={{ display:"inline-block", marginTop:"15px", marginRight:"15px", width:"450px", textAlign:"center"}}>
+			<h2 style={{  textAlign:"left"}}>Hope Public Charitable Trust</h2>
+			<p style={{ textAlign:"left"}}>
 				6b/26, Sarangapani Street, Krishnapuram, Ambattur
 				Chennai - 600053
 
 			</p>
-			<div style={{ width: "500px", margin:"auto"}}>
+			<div>
 				<a style={{ color: "rgb(80, 183, 255)"}} href="https://hopechennai.com/our-services/"> Our Services | Hope Chennai Trust </a>
 			</div>	
 		</div>
-		<div style={{ display:"inline-block", marginTop:"15px", marginLeft:"15px", width:"35%", textAlign:"center"}}>
-			<h2 style={{ width: "500px", margin:"auto", textAlign:"left" }}>Samarthanam Trust for the Disabled</h2>
-			<p style={{ width: "500px", margin:"auto", textAlign:"left" }}>
+		<div style={{ display:"inline-block", marginTop:"15px", marginLeft:"15px", width:"450px", textAlign:"center"}}>
+			<h2 style={{ textAlign:"left" }}>Samarthanam Trust for the Disabled</h2>
+			<p style={{ textAlign:"left" }}>
 				CA:39, 15th Cross, 16th Main, Sector-4, 
 				HSR Layout, Bangalore - 560102
 			</p>
-			<div style={{ width: "500px", margin:"auto"}}>
+			<div>
 				<a style={{ color: "rgb(80, 183, 255)" }} href="https://www.samarthanam.org/about-us/"> About Us | Samarthanam Trust for the Disabled | NGO for Disabled </a> 
 			</div>
 	       </div>
